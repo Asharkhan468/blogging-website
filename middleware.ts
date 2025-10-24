2320e8cfecf760433a2e4146d9762a1bc0bcb30c
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-10-24 18:28:39 +0500

    SOME CODE ADD

--- middleware.ts
+++ middleware.ts
@@ -1,49 +1,19 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-
-// export function middleware(req: NextRequest) {
-//   const token = req.cookies.get("token")?.value;
-//   console.log("Token:", token);
-//   const { pathname } = req.nextUrl;
-
-//   // Allow public routes (like login and register)
-//   if (pathname.startsWith("/auth/login") || pathname.startsWith("/auth/register") || pathname.startsWith("/api")) {
-//     return NextResponse.next();
-//   }
-
-//   // If no token -> redirect to login
-//   if (!token) {
-//     const loginUrl = new URL("/auth/login", req.url);
-//     loginUrl.searchParams.set("redirectTo", pathname);
-//     return NextResponse.redirect(loginUrl);
-//   }
-
-//   return NextResponse.next();
-// }
-
-// // Run middleware for all routes
-// export const config = {
-//   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-// };
-
-
-
-
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   // const token = req.cookies.get("token")?.value;
-    const cookieHeader = req.headers.get("cookie");
+  const cookieHeader = req.headers.get("cookie");
   const token =
     req.cookies.get("token")?.value ||
     (cookieHeader
-      ? cookieHeader.split("; ").find((c) => c.startsWith("token="))?.split("=")[1]
+      ? cookieHeader
+          .split("; ")
+          .find((c) => c.startsWith("token="))
+          ?.split("=")[1]
       : null);
   const { pathname } = req.nextUrl;
 
- 
   // Public routes allow
   if (
     pathname.startsWith("/auth/login") ||
