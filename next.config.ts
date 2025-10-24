cbec6d89dcb1c6aaa081bca5e9e41afb19aa5b33
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-10-24 17:56:37 +0500

    ADD SOME CHANGES IN NEXT CONFIG FILE

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+import type { NextConfig } from "next";
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/server/:path*", // proxy path
+        destination: "https://blogging-website-server-rose.vercel.app", // ← backend ka URL likho yahan
+      },
+    ];
+  },
 };
 
 export default nextConfig;
